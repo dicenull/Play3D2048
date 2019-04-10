0a68480978dbc4106f6243737ce9a2494149c5cb
s1500729 <s1500729@sendai-nct.jp>
2019-04-10 21:06:43 +0900

    領域全体を囲った

--- Assets/Scripts/BoxBuilder.cs
+++ Assets/Scripts/BoxBuilder.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class BoxBuilder : MonoBehaviour
 {
-	private GameObject box;
 
 	private void Start()
 	{
-		box = Resources.Load<GameObject>("Box");
+		var box = Resources.Load<GameObject>("Box");
+		var areaBox = Resources.Load<GameObject>("AreaBox");
+		
 
 		var vec = new[] { -1, 0, 1 };
+		var scale = 2f;
+
+		var length = vec.Length + (scale / 2) * (vec.Length - 1);
+		var areaBoxObj = Instantiate(areaBox, Vector3.zero, Quaternion.identity, transform);
+
+		areaBoxObj.transform.localScale = Vector3.one * length;
+
 		foreach(var x in vec)
 		{
 			foreach(var y in vec)
 			{
 				foreach(var z in vec)
 				{
-					var pos = new Vector3(x, y, z) * 2;
+					var pos = new Vector3(x, y, z) * scale;
 
 					Instantiate(box, pos, Quaternion.identity, transform);
 				}
